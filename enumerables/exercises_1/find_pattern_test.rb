c758aeb971ee5c5a4741815c69c5f0fbaf6d7331
Logan Riffell <lkriffell@gmail.com>
2020-06-26 10:09:38 -0600

    Add find_pattern_test

--- enumerables/exercises_1/find_pattern_test.rb
+++ enumerables/exercises_1/find_pattern_test.rb
@@ -21,36 +21,67 @@ class FindPatternTest < Minitest::Test
     found = nil
     words.each do |word|
       # Your code goes here
+      if word == "waldo"
+        found = word
+        break
+      end
     end
     assert_equal nil, found
   end
 
   def test_find_waldo
-    skip
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
     found = nil
     # Your code goes here
+    words.each do |word|
+      # Your code goes here
+      if word == "waldo"
+        found = word
+        break
+      end
+    end
     assert_equal "waldo", found
   end
 
   def test_cannot_find_3_letter_words
-    skip
     words = ["piglet", "porridge", "bear", "blueberry"]
     # Your code goes here
+    found = nil
+    words.each do |word|
+      # Your code goes here
+      if word.length == 3
+        found = word
+        break
+      end
+    end
     assert_equal nil, found
   end
 
   def test_find_13
-    skip
     numbers = [2, 13, 19, 8, 3, 27]
     # Your code goes here
+    found = nil
+    numbers.each do |num|
+      # Your code goes here
+      if num == 13
+        found = num
+        break
+      end
+    end
     assert_equal 13, found
   end
 
   def test_find_first_even_number
-    skip
     numbers = [3, 7, 13, 11, 10, 2, 17]
     # Your code goes here
+    found = nil
+    numbers.each do |num|
+      # Your code goes here
+      if num % 2 == 0
+        found = num
+        break
+      end
+    end
     assert_equal 10, found
   end
 
@@ -58,20 +89,40 @@ class FindPatternTest < Minitest::Test
     skip
     numbers = [2, 8, 9, 27, 24, 5]
     # Your code goes here
+    found = nil
+    numbers.each do |num|
+      # Your code goes here
+      if num % 3 == 0
+        found = num
+        break
+      end
+    end
     assert_equal 9, found
   end
 
   def test_find_first_word_starting_with_q
-    skip
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
     # Your code goes here
+    found = nil
+    words.each do |word|
+      if word.split('').first == 'q'
+        found = word
+        break
+      end
+    end
     assert_equal "quill", found
   end
 
   def test_find_first_word_ending_with_er
-    skip
     words = ["biggest", "pour", "blight", "finger", "pie", "border"]
     # Your code goes here
+    found = nil
+    words.each do |word|
+      if word.split('').last(2) == ["e", "r"]
+        found = word
+        break
+      end
+    end
     assert_equal "finger", found
   end
 
@@ -79,6 +130,11 @@ class FindPatternTest < Minitest::Test
     skip
     numbers = [1, 8, 19, 21, 29, 31, 34]
     # Your code goes here
+    numbers.each do |x|
+      if x > 20
+        found = x
+      end
+    end
     assert_equal 21, found
   end
 
