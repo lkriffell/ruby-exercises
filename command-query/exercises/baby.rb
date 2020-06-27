d53c83f734e2dd12f312ffd29b3578cda87b0ce0
Logan Riffell <lkriffell@gmail.com>
2020-06-27 12:51:32 -0600

    Add baby test

---
+++ command-query/exercises/baby.rb
@@ -0,0 +1,18 @@
+class Baby
+  attr_reader :tired
+  def initialize(tired = true)
+    @tired = tired
+  end
+
+  def nap
+    tired = false
+  end
+
+  def tired?
+    if tired == true
+      "The baby is tired."
+    elsif tired == false
+      "Naps help combat drowsiness."
+    end
+  end
+end
